288c8b4ff58b670249da57b743ab15f81526f113
minjae0402 <kangjack0402@gmail.com>
2025-11-26 23:56:51 +0900

    Fix : urls.py, settings.py

--- uploader/views.py
+++ uploader/views.py
@@ -1,45 +1,51 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.generics import get_object_or_404
 from .models import SaveFile
 from .serializers import SaveFileSerializer
-import json
 
-# Create your views here.
 @api_view(['GET', 'POST', 'DELETE'])
-def uploaderAPI(request):
-    try:
-        username = request.data["username"]
-        gameID = request.data["gameID"]
-        fileName = request.data["fileName"]
-    except:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+def uploaderAPI(request, username=None, fileName=None):
+
     if request.method == 'GET':
-        try:
-            file = SaveFile.objects.get(username=username, gameID=gameID, fileName=fileName)
-            serializer = SaveFileSerializer(file)
+        if username and fileName:
+            try:
+                file = SaveFile.objects.get(username=username, fileName=fileName)
+                serializer = SaveFileSerializer(file)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except SaveFile.DoesNotExist:
+                return Response({"error": "File not found"}, status=status.HTTP_404_NOT_FOUND)
+        elif username:
+            files = SaveFile.objects.filter(username=username)
+            serializer = SaveFileSerializer(files, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            files = SaveFile.objects.all()
+            serializer = SaveFileSerializer(files, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
     if request.method == 'POST':
         try:
+            username = request.data["username"]
+            gameID = request.data["gameID"]
+            fileName = request.data["fileName"]
             saveData = request.data["saveData"]
-        except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        try:
-            file = SaveFile.objects.get(username=username, gameID=gameID, fileName=fileName)
-            file.saveData = saveData
-            file.save()
-            return Response(status=status.HTTP_200_OK)
-        except:
-            file = SaveFile(username=username, gameID=gameID, fileName=fileName, saveData=saveData)
-            file.save()
-            return Response(status=status.HTTP_201_CREATED)
+        except KeyError:
+            return Response({"error": "Missing required fields"}, status=status.HTTP_400_BAD_REQUEST)
+
+        file, created = SaveFile.objects.update_or_create(
+            username=username, gameID=gameID, fileName=fileName,
+            defaults={'saveData': saveData}
+        )
+        return Response(
+            {"status": "created" if created else "updated"},
+            status=status.HTTP_201_CREATED if created else status.HTTP_200_OK
+        )
+
     if request.method == 'DELETE':
         try:
-            file = SaveFile.objects.get(username=username, gameID=gameID, fileName=fileName)
+            file = SaveFile.objects.get(username=username, fileName=fileName)
             file.delete()
-            return Response(status=status.HTTP_200_OK)
-        except:
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response({"status": "deleted"}, status=status.HTTP_200_OK)
+        except SaveFile.DoesNotExist:
+            return Response({"error": "File not found"}, status=status.HTTP_404_NOT_FOUND)
